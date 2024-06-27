35d4fd2b25ad25e6926e0427c220539b9bac71b6
bazhenovairinaa <145771526+bazhenovairinaa@users.noreply.github.com>
2024-06-27 13:29:33 +0300

    Add files via upload

--- javascripts/preloader.js
+++ javascripts/preloader.js
@@ -1,11 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   window.addEventListener("load", function () {
-//     setTimeout(function () {
-//       let preloader = document.getElementById("preloader");
-//       preloader.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let preloader = document.getElementById("preloader");
+      preloader.style.display = "none";
 
-//       let content = document.getElementById("content");
-//       content.style.display = "block";
-//     }, 3000);
-//   });
-// });
+      let content = document.getElementById("content");
+      content.style.display = "block";
+    }, 3000);
+  });
+});
